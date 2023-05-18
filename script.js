8a319c2366f7f6525c2f7dc37316cdf73daec70b
Phelipe Afonso <phelipeafonso.freitas@gmail.com>
2023-05-18 07:19:33 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("Mandando oi para o amigo!");
+
+function mandaMensagem() {
+    console.log("Tudo bem?");
+    console.log("Vou te mandar uma solicitação");
+    console.log("Solicitação recebida!");
+}
+
+setTimeout(() => {
+    mandaMensagem();
+}, 5000);
+
+console.log("Tchau tchau");
